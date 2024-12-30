89acfb308f00586f2a5c49e99598e8d192a5e9a0
chris <root@ChrisDell>
2024-12-30 14:49:24 +0200

    screenshot and update

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-demo',
-    version: '0.0.1',
+    version: '0.0.2',
     icon: 'i',
     title: 'Demo plugin',
     description: 'This is a demo plugin',
